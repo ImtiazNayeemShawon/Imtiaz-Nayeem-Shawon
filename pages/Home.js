969f1464d897c141da2d2300b81f8066280731fe
Imtiaz Nayeem Shawon <89548269+ImtiazNayeemShawon@users.noreply.github.com>
2023-02-28 22:59:25 +0600

    Update Home.js
    Change about me

--- pages/Home.js
+++ pages/Home.js
@@ -24,11 +24,7 @@ export default function Home() {
           </div>
           <p className="text-gray-700 leading-7 text-left text-sm  mt-8 max-sm:text-justify">
             {" "}
-            I am a creative and ambitious web developer. I have been doing web
-            design for the last 2 years and I have worked in 7+ organizations as
-            a web developer and also done some client work. I am expert in
-            Javascript Next js and React js also proficient in React js tailwind
-            CSS MUI and CSS.
+            My name is Imtiaz Nayeem Shawon, and I am a student of Cumilla Modern High School, SSC 23 batch. I am passionate about full-stack web development and am also learning AI and data science. My love for technology has led me to participate in many Olympiads, such as the Bangladesh Junior Science Olympiad and the ICT Olympiad. In 2021, I was honored with a Creative Junior Award for my web designing skills. I am constantly learning and strive to explore new things in the field of technology.
           </p>
           <div className="flex max-sm:justify-around">
             <button className="mt-6 bg-gray-800  text-white px-7 p-3 rounded-lg uppercase font-semibold hover:bg-white hover:text-gray-800 duration-300 hover:shadow-xl">
