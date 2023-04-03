624d24b0ff44a74e2d03a22e8d55c400c5016d11
Imtiaz Nayeem Shawon <89548269+ImtiazNayeemShawon@users.noreply.github.com>
2023-04-03 22:11:16 +0000

    wdfsf

--- pages/Home.js
+++ pages/Home.js
@@ -4,46 +4,60 @@ import Imtiaz from "../public/imti.png";
 
 export default function Home() {
   return (
-    <div className="bghome ">
+    <div className="bghome">
       <div className="grid grid-cols-2 max-sm:grid-cols-1  place-items-center mx-20  max-sm:mx-5 ">
-        <div className="mt-20">
-          
-          <h1 className="leading-tight text-4xl font-bold  mt-10  text-gray-700 max-sm:mt-1 max-sm:text-center max-sm:text-xl">
-            It's a pleasure to introduce myself
+        <div className="mt-40">
+          <h1 className="leading-tight text-7xl font-medium capitalize text-black max-sm:mt-1 max-sm:text-center max-sm:text-4xl mainfont">
+            Imtiaz Nayeem
           </h1>
-          <h1 className="leading-tight   text-6xl font-bold uppercase text-blue-700 max-sm:mt-1 max-sm:text-center max-sm:text-4xl">
-            <span className="text-red-500  ">I'm</span> Imtiaz Nayeem Shawon
-          </h1>
+          <div className="flex justify-content mt-10">
+           
+         <div> <h2 className="text-2xl mainfont font-semibold capitalize job">web Developer</h2></div>
+
+          </div>
           <div className="">
-            <Image
+            {/* <Image
               className="m-auto  hidden max-sm:block"
               src={Imtiaz}
               width="500"
               alt="imtiaz nayeem shawon"
-            />
+            /> */}
           </div>
-          <p className="text-gray-700 leading-7 text-left text-sm  mt-8 max-sm:text-justify">
+          <p className="text-gray-700 leading-10 mr-40 max-sm:mr-0 text-left text-xl  mt-8 max-sm:text-justify font-bold ">
             {" "}
-            My name is Imtiaz Nayeem Shawon, and I am a student of Cumilla Modern High School, SSC 23 batch. I am passionate about full-stack web development and am also learning AI and data science. My love for technology has led me to participate in many Olympiads, such as the Bangladesh Junior Science Olympiad and the ICT Olympiad. In 2021, I was honored with a Creative Junior Award for my web designing skills. I am constantly learning and strive to explore new things in the field of technology.
+            I'm creative designer based in New York, and I'm very passionate and
+            dedicated to my work.
           </p>
-          <div className="flex max-sm:justify-around">
-            <button className="mt-6 bg-gray-800  text-white px-7 p-3 rounded-lg uppercase font-semibold hover:bg-white hover:text-gray-800 duration-300 hover:shadow-xl">
-              Resume
+          <div className="flex max-sm:justify-around gap-20 mt-20">
+            <button className="mt-6 flex justify-around bg-blue-700 text-2xl text-white px-16 py-6 rounded-3xl capitalize font-semibold hover:bg-white hover:text-gray-800 duration-300 hover:shadow-sm">
+              Resume{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+                />
+              </svg>
             </button>
-            <button className="mt-6 bg-gray-800  text-white px-7 p-3 rounded-lg uppercase font-semibold hover:bg-white hover:text-gray-800 duration-300 hover:shadow-xl ml-20">
-              Contact{" "}
+            <button className="mt-6 flex justify-around bg-teal-100  text-2xl text-gray-800  px-16 py-6 rounded-3xl capitalize font-semibold hover:bg-blue-700 hover:text-white duration-300 hover:shadow-sm">
+              My works
             </button>
-            
-           
           </div>
         </div>
         <div className="">
-          <Image
-            className="m-auto block max-sm:hidden"
-            src={Imtiaz}
-            width="500"
-            alt="imtiaz nayeem shawon"
-          />
+          {/* <Image
+              className="m-auto  hidden max-sm:block"
+              src={Imtiaz}
+              width="500"
+              alt="imtiaz nayeem shawon"
+            /> */}
         </div>
       </div>
     </div>
